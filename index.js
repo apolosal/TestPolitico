91f1949cf9e25e9433623007992e7a5666f2b2b1
apolosal <apolo1@gmail.com>
2025-10-21 22:18:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import React, { useState, useEffect } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+// (Aquí va todo el código completo de TestPoliticoMVP.jsx que te pasé antes)
+
+ReactDOM.render(<TestPoliticoMVP />, document.getElementById("root"));
